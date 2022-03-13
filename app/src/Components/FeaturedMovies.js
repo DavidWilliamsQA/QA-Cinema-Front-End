a6de445265089676fd61ca407c169a38b245ad03
DavidWilliamsQA <60790661+DavidWilliamsQA@users.noreply.github.com>
2022-03-13 01:20:37 +0000

    added a main and featuredmovies component added more stuff from the css of the old project added bootstrap to the page added some fonts used the props functionality to populate different featured movies cards updated the nav bar too have different items in it

---
+++ app/src/Components/FeaturedMovies.js
@@ -0,0 +1,45 @@
+import React from "react"
+import "../style.css"
+// import "../bootstrap-grid.min.css"
+
+export default function FeaturedMovies(props){
+    
+    return (
+
+        <section className="home">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <div className="card">
+                            <div className="card__cover">
+                                <img src={props.img} alt="" />
+                                <a id="play512200" href="www.google.com" className="card__play">
+                                    <i className="icon ion-ios-play"></i>
+                                </a>
+                            </div>
+                            <div className="card__content">
+                                    <h3 className="card__title">
+                                        <a id="title512200" href="www.google.com">{props.title}</a>
+                                    </h3>
+                                    <span className="card__category">
+                                                genres 
+                                            </span>
+                                            <div className="card__wrap">
+                                                <ul className="card__list">
+                                                    <li>{props.releaseDate}</li>
+                                                    <li><a  id="ageRating512200" href="www.google.com">{props.rating}</a></li>
+                                                </ul>
+                                            </div>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+
+
+        
+    )
+}
---
+++ app/src/Components/Main.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "../style.css"
+
+export default function Main(){
+    return (
+        <section>
+            <main className="main--body">
+                <h2>Main Body</h2>
+                <h2>Main Body</h2>
+                <h2>Main Body</h2>
+            </main>
+        </section>
+        
+    )
+}
