e0c9b35b852e786d67a7abe3f71a61613cb2bd4e
DavidWilliamsQA <60790661+DavidWilliamsQA@users.noreply.github.com>
2022-03-14 20:11:34 +0000

    fixed the issue with the movie tiles not showing up side by side the issue was with the width being fixed at 200px added some section code to the enclose the Featured Movie components with the props in them

--- app/src/Components/FeaturedMovies.js
+++ app/src/Components/FeaturedMovies.js
@@ -6,40 +6,30 @@ export default function FeaturedMovies(props){
     
     return (
 
-        <section className="home">
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <div className="card">
-                            <div className="card__cover">
-                                <img src={props.img} alt="" />
-                                <a id="play512200" href="www.google.com" className="card__play">
-                                    <i className="icon ion-ios-play"></i>
-                                </a>
-                            </div>
-                            <div className="card__content">
-                                    <h3 className="card__title">
-                                        <a id="title512200" href="www.google.com">{props.title}</a>
-                                    </h3>
-                                    <span className="card__category">
-                                                genres 
-                                            </span>
-                                            <div className="card__wrap">
-                                                <ul className="card__list">
-                                                    <li>{props.releaseDate}</li>
-                                                    <li><a  id="ageRating512200" href="www.google.com">{props.rating}</a></li>
-                                                </ul>
-                                            </div>
-                                </div>
-                        </div>
+        <div className="col-6 col-sm-4 col-lg-3 col-xl-2">
+            <div className="card">
+                <div className="card__cover">
+                    <img src={props.img} alt="" />
+                    <a id="play512200" href="www.google.com" className="card__play">
+                        <i className="icon ion-ios-play"></i>
+                    </a>
+                </div>
+                <div className="card__content">
+                    <h3 className="card__title">
+                        <a id="title512200" href="www.google.com">{props.title}</a>
+                    </h3>
+                    <span className="card__category">
+                        genres 
+                    </span>
+                    <div className="card__wrap">
+                        <ul className="card__list">
+                            <li>{props.releaseDate}</li>
+                            <li><a  id="ageRating512200" href="www.google.com">{props.rating}</a></li>
+                        </ul>
                     </div>
                 </div>
-
             </div>
-
-        </section>
-
-
+        </div>
         
     )
 }
